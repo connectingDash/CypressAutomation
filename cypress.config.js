b752a6ddc892e9c8fb4c1193220c65dd0c118644
Abhishek Anand <anandabhishek231@gmail.com>
2023-01-23 21:54:12 +0530

    Updated config.js file

--- cypress.config.js
+++ cypress.config.js
@@ -1,5 +1,17 @@
 const { defineConfig } = require("cypress");
+const { defineConfig } = require("cypress");
+const preprocessor = require("@badeball/cypress-cucumber-preprocessor");
+const browserify = require("@badeball/cypress-cucumber-preprocessor/browserify");
 
+async function setupNodeEvents(on, config) {
+  // This is required for the preprocessor to be able to generate JSON reports after each run, and more,
+  await preprocessor.addCucumberPreprocessorPlugin(on, config);
+
+  on("file:preprocessor", browserify.default(config));
+
+  // Make sure to return the config object as it might have been modified by the plugin.
+  return config;
+}
 module.exports = defineConfig({
   defaultCommandTimeout: 8000,
   pageLoadTimeout: 3000,
@@ -9,11 +21,8 @@ module.exports = defineConfig({
   },
   projectId: "zk6hxv",
   e2e: {
-    async setupNodeEvents(on, config) {
-      // implement node event listeners here
-      await preprocessor.addCucumberProcessorPlugin(on, config);
-      on("file:preprocessor", browserify.default(config));
-    },
+    specPattern: "**/*.feature",
+    setupNodeEvents,
     specPattern: 'cypress/integration/examples/*.js'
-  },
+  }
 });
